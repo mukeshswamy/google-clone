166fa2f9897160a64ada1b842fc0b9876dcea6a1
mukeshswamy <59552894+mukeshswamy@users.noreply.github.com>
2020-12-12 01:54:06 +0530

    pages-added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      This is Google
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {/* HOME (The one with the search bar) */}
+            <Home />
+          </Route>
+          <Route path="/search" exact>
+            <h1>Hello</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__header">
+        <div className="home__headerLeft">
+          {/* Link */}
+          {/* Link */}
+        </div>
+        <div className="home__headerRight">
+          {/* Link */}
+          {/* Link */}
+          {/* Icon */}
+          {/* Avatar */}
+        </div>
+      </div>
+      <div className="home__body"></div>
+    </div>
+  );
+}
+
+export default Home;
